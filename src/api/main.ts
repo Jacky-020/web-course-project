fb652abffaf79650328efd0482881e54cc4ff31b
FieryRMS <raiyanmohdshahrms@gmail.com>
2024-12-09 23:21:57 +0800

    use setGlobalPrefix instead

--- src/api/main.ts
+++ src/api/main.ts
@@ -10,6 +10,7 @@ db.once('open', async () => {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new ExpressAdapter());
+  app.setGlobalPrefix('api');
   await app.init();
   ViteExpress.listen(app.getHttpAdapter().getInstance(), 3000);
 }
