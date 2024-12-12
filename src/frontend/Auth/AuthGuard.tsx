b9f69869b477cdb28d1d0bd9c83a46a72591ed4c
FieryRMS <raiyanmohdshahrms@gmail.com>
2024-12-12 17:39:10 +0800

    frontend AuthGuard

---
+++ src/frontend/Auth/AuthGuard.tsx
@@ -0,0 +1,32 @@
+import React, { ReactNode } from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from './AuthProviderHooks';
+
+interface AuthGuardProps {
+    noAuth?: boolean;
+    roles?: string[];
+    children: ReactNode;
+}
+
+const AuthGuard: React.FC<AuthGuardProps> = (props) => {
+    const state = useAuth();
+
+    const navEl = (
+        <Navigate
+            to={{
+                pathname: '/login',
+                search: '?redirect=' + window.location.pathname,
+            }}
+            state={{ AuthError: 'You must be logged in!' }}
+            replace
+        />
+    );
+
+    if (props.noAuth) return props.children;
+    if (state.loading || !state.init) return <>Loading</>;
+    if (!state.user) return navEl;
+    if (props.roles && !props.roles.every((role) => state.user?.roles.includes(role))) return <h1>Unauthorized</h1>;
+    return props.children;
+};
+
+export default AuthGuard;
