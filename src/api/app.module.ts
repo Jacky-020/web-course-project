9a4de23d975f45f9bc56b2cee8c422b2d289a969
FearlessSniper <47030588+FearlessSniper@users.noreply.github.com>
2024-12-13 21:40:07 +0800

    Make MongoDB connection URL a configuration

--- src/api/app.module.ts
+++ src/api/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { APP_GUARD } from '@nestjs/core';
@@ -15,12 +15,17 @@ import { RoleGuard } from './auth/auth.guard';
       envFilePath: ['.env', '.env.dev'],
     }),
     AuthModule,
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/', {
-      onConnectionCreate: (connection) => {
-        connection.on('connected', () =>
-          Logger.log('Connected to database', 'MongoDB'),
-        );
-      },
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('MONGO_URL', 'mongodb://127.0.0.1:27017/'),
+        onConnectionCreate: connection => {
+          connection.on('connected', () => 
+            Logger.log('Connected to database', 'MongoDB'),
+          );
+        }
+      }),
+      inject: [ConfigService],
     }),
     UserModule,
   ],
